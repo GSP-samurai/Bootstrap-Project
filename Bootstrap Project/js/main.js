f5e5be1bd76188489a45187e7b1bce9bfd1393ef
Alex <68346370+GSP-samurai@users.noreply.github.com>
2021-03-29 19:06:42 +0300

    Add files via upload

---
+++ Bootstrap Project/js/main.js
@@ -0,0 +1,49 @@
+$(function() {
+    
+    $('.services-carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        navText:['<img src="img/left.svg">', '<img src="img/right.svg">'],
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1.33
+            }
+        }
+    });
+
+    $('.testimonials-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText:['<img src="img/left.svg">', '<img src="img/right.svg">'],
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function(e) {
+        e.preventDefault();
+
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('section.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+      });
+
+});
